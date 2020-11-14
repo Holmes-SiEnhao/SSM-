151d3459568cc1859be904123955587dd1b88b76
Holmes <2296026227@qq.com>
2020-11-14 10:58:21 +0800

    lombok使用

--- mybatisTest/src/main/java/com/demo/pojo/User.java
+++ mybatisTest/src/main/java/com/demo/pojo/User.java
@@ -1,37 +1,20 @@
 package com.demo.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author: seh
  * @date: 2020/10/25 9:04
  * @version: 1.0
  */
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private int id;
     private String username;
     private String password;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
